d06448cc160152719124d625a4084597b0ed59bd
DenrianWeiss <this@kunagisa.moe>
2025-03-17 15:40:05 +0800

    Add pages and stubs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,31 +6,25 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'My Docs',
-          social: {
-              github: 'https://github.com/withastro/starlight',
-          },
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-          ],
-      }),
-	],
+    integrations: [
+        starlight({
+            title: 'Explosive Computers',
+            defaultLocale: 'zh-cn',
+            social: {
+                github: 'https://github.com/DenrianWeiss/explosive',
+            },
+            sidebar: [
+                {
+                    label: '厂商',
+                    autogenerate: { directory: 'vendors' }
+                }
+            ],
+        }),
+    ],
 
-  adapter: cloudflare({
-      platformProxy: {
-          enabled: true
-      }
-  }),
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true
+        }
+    }),
 });
---
+++ src/content/docs/vendors/huawei.md
@@ -0,0 +1,36 @@
+---
+title: 华为
+description: 看看都有谁造的炸弹
+---
+
+# 产品列表
+
+## 移动 SoC
+
+参考： [麒麟芯片](https://www.hisilicon.com/cn/products/Kirin)
+
+- 麒麟 990
+- 麒麟 9000
+- 麒麟 9000S
+- 麒麟 9010
+- 麒麟 9020
+
+## 服务器处理器
+
+参考：[鲲鹏社区](https://www.hikunpeng.com/)
+
+- Hi1612(鲲鹏912)
+- Hi1616(鲲鹏916)
+- Hi1620(鲲鹏920)
+    - [920V1](/products/vendors/huawei/soc/hi1620.md)
+    - [920V2](/products/vendors/huawei/soc/hi1630.md)
+
+## 加速器及计算卡
+
+参考：[昇腾社区](https://www.hiascend.com/)
+
+- Ascend 310(昇腾310)
+    - Orange Pi AI Pro
+    - Orange Pi AI Studio (Pro)
+- Ascend 910(昇腾910)
+
